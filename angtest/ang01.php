a4b6be72d9b3843c496cc8a843ca02d4164b0c03
wahhid <wahhid@wahhid-pc>
2015-01-01 22:09:16 +0700

    Init

---
+++ angtest/ang01.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<script src="../js/angular.min.js"></script>	
+</head>
+<body>
+<div ng-app="">
+	<p>Name : <input type="text" ng-model="name"></p>
+	<p ng-bind="name"></p> 
+</div>
+</body>
+</html>
